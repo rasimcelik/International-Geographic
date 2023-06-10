b17e3fe1c14b60f9e1cc886a104f58e78f6b9268
rasimcelik <rasimcelik.rc@gmail.com>
2023-06-10 19:06:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const buttons = document.getElementsByClassName("buttons");
+
+const continents = [
+  "Africa",
+  "Antarctica",
+  "Asia",
+  "Australia",
+  "Europe",
+  "North America",
+  "South America",
+];
+
+document.getElementById("africa").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/africa.html";
+  },
+  false
+);
+document.getElementById("antarctica").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/antarctica.html";
+  },
+  false
+);
+document.getElementById("asia").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/asia.html";
+  },
+  false
+);
+document.getElementById("australia").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/australia.html";
+  },
+  false
+);
+document.getElementById("europe").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/europe.html";
+  },
+  false
+);
+document.getElementById("na").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/na.html";
+  },
+  false
+);
+document.getElementById("sa").addEventListener(
+  "click",
+  function () {
+    location.href = "/pages/sa.html";
+  },
+  false
+);
